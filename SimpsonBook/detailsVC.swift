fb9fba4ab661add18cee5cad605cd7f11623af0f
Busra Gazel <busragzl96@gmail.com>
2025-01-22 22:26:33 +0300

    viewların baglanması

---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,27 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by Musa on 22.01.2025.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
