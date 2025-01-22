62fafe26f191f173e107ef6dfcb9e2bbc0b06954
Busra Gazel <busragzl96@gmail.com>
2025-01-22 23:48:30 +0300

    simpslar için tableView oluşturma ve bağlama

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Musa on 22.01.2025.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+    
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -2,19 +2,56 @@
 //  ViewController.swift
 //  SimpsonBook
 //
-//  Created by Musa on 22.01.2025.
+//  Created by Büşra Gazel on 22.01.2025.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource{
 
    
     @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpsons = [Simpson]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        //Simpson Object
+        
+        let homer = Simpson(simpsonName: "Homer Simpson", simpsonJob:"Nuclear Safety", simpsonImage:UIImage(named: "homer")!)
+        let marge = Simpson(simpsonName: "Marge Simpson", simpsonJob:"House Wife", simpsonImage:UIImage(named: "marge")!)
+        let bart = Simpson(simpsonName: "Bart Simpson", simpsonJob:"Student", simpsonImage:UIImage(named: "bart")!)
+        let lisa = Simpson(simpsonName: "Lisa Simpson", simpsonJob:"Student", simpsonImage:UIImage(named: "lisa")!)
+        let maggie = Simpson(simpsonName: "Maggie Simpson", simpsonJob:"Baby", simpsonImage:UIImage(named: "maggie")!)
+        
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(marge)
+        mySimpsons.append(bart)
+        mySimpsons.append(lisa)
+        mySimpsons.append(maggie)
+        
     }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return mySimpsons.count // Kaç satırlı olsun
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell() // Hücre oluşturma
+        cell.textLabel?.text = mySimpsons[indexPath.row].name
+        return cell
+    }
+    
+    
+    
 
 
 }
